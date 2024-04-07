18dc7356816cccdd6a1cc94bbe59d201d6c15a7b
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-04-07 18:34:24 -0400

    created object for blog entries in js and stored the string version in local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,16 @@
-submitButton = document.getElementById("submit-post");
-
 function submitEntry (){
     console.log('yeah that worked')
+    const blogEntries = {
+        usernameEntry: username.value,
+        titleEntry: title.value,
+        contentEntry: content.value.trim()
+    }
+    console.log(blogEntries);
+    localStorage.setItem('blogEntry',JSON.stringify(blogEntries));
 }
 
+submitButton = document.getElementById("submit-post");
+username = document.getElementById("username");
+title = document.getElementById("title");
+content = document.getElementById("content");
 submitButton.addEventListener("click", submitEntry)
