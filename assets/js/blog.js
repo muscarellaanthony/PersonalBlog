dd99129860eba17735232ac6a622cd935dcfb743
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-04-08 18:26:27 -0400

    created js divs to display post info on the blog page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,8 +1,30 @@
 const displayNewPost = function (){
-    const stringContent = localStorage.getItem('blogEntry')
+    const stringContent = localStorage.getItem('blogEntries')
     const newPost = JSON.parse(stringContent)
+    const post = document.getElementById('post')
 
     console.log(newPost)
+    for(i = 0; i < newPost.length; i++){
+        let div = document.createElement('div')
+        let title = document.createElement('h2')
+        let content = document.createElement('p')
+        let author = document.createElement('h3')
+
+        title.textContent = newPost[i].titleEntry;
+        content.textContent = newPost[i].contentEntry;
+        author.textContent = 'Posted by: ' + newPost[i].usernameEntry;
+
+        div.setAttribute('class', 'blog-div');
+        title.setAttribute('class', 'blog-title');
+        content.setAttribute('class', 'blog-content');
+        author.setAttribute('class', 'blog-author');
+
+        div.append(title);
+        div.append(content);
+        div.append(author);
+
+        post.append(div);
+    }
 }
 
 
